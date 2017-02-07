4e584ab1ca4598a642ad8b04fd6bb5d7c29c1e6e
Artem Yavorsky <aqson@me.com>
2017-02-07 15:57:37 +0200

    Fixes

--- topics.js
+++ topics.js
@@ -22,16 +22,25 @@
     fetch(jsonPath).then(d => {
       d.json().then(data => {
         const {keywords} = data;
+
         document.querySelector(".btn-link.js-repo-topics-form-toggle.js-details-target").click();
+        if (!keywords || !keywords.length) return;
+
         let interval;
+
         const check = () => {
           $formUl = document.querySelector("#repo-topics-edit-form .js-tag-input-selected-tags");
-          console.log($formUl);
           if (!$formUl) return;
+
+          const formValues = Array.from($formUl.querySelectorAll('input')).map(item => item.value);
+
           clearInterval(interval);
+
           keywords.forEach(keyword => {
+            if (formValues.includes(keyword)) return;
             $formUl.appendChild(createTopic(keyword));
           });
+
           // $form.submit();
         }
         interval = setInterval(check, 1000)
@@ -39,40 +48,45 @@
     })
   }
 
-  const onStateChange = () => {
-    const $topics = document.querySelector("#topics-list-container");
+  const onStateChange = ($topics, $addTopics, $jsonFile) => {
+    if (!$topics) {
+      $topics = document.querySelector("#topics-list-container");
+    }
     const $topicsSync = document.getElementById("topics-sync");
     if (!$topics || $topicsSync) return;
-    const isEditable = $topics.querySelector("button.js-repo-topics-form-toggle");
-    const jsonFile = document.querySelector(".file-wrap .files td span a[title='package.json']");
-    if (isEditable && jsonFile) {
+
+    if (!$addTopics) {
+      $addTopics = $topics.querySelector("button.js-repo-topics-form-toggle");
+    }
+    if (!$jsonFile) {
+      $jsonFile = document.querySelector(".file-wrap .files td span a[title='package.json']");
+    }
+    if ($addTopics && $jsonFile) {
       const $sync = document.createElement("button");
       $sync.id = "topics-sync";
       $sync.className = "btn-link bth-sync";
       $sync.innerHTML = "Fetch from package.json";
       $topics.appendChild($sync);
-      $sync.addEventListener("click", handleSyncClicked.bind(null, jsonFile.href));
+      $sync.addEventListener("click", handleSyncClicked.bind(null, $jsonFile.href));
     }
   }
 
-  const observer = new MutationObserver(function(mutations){
-    const formChanged = mutations.some(mutation => {
-      const added = Array.from(mutation.addedNodes).some(node => {
-        return node.classList && node.classList.contains('js-topic-form-area');
-      });
-      const removed = Array.from(mutation.removedNodes).some(node => {
-        return node.classList && node.classList.contains('js-topic-form-area');
-      });
-      return added || removed;
+  const $topics = document.querySelector("#topics-list-container");
+  const $addTopics = $topics.querySelector("button.js-repo-topics-form-toggle");
+  const $jsonFile = document.querySelector(".file-wrap .files td span a[title='package.json']");
+
+  if ($addTopics && $jsonFile) {
+    const observer = new MutationObserver(function(mutations){
+      const classChanged = mutations.every(mutation => {
+        return mutation.attributeName = 'class';
+      })
+      const $topicsSync = document.getElementById("topics-sync");
+      if (classChanged && !$topicsSync) {
+        onStateChange();
+      }
     });
-    const $topicsSync = document.getElementById("topics-sync");
+    observer.observe($topics, { attributes: true, childList: true, characterData: true });
 
-    if (formChanged && !$topicsSync) {
-      onStateChange();
-    }
-  });
-  const $topicsContainer = document.querySelector('.js-repository-topics-container');
-  observer.observe($topicsContainer, { attributes: true, childList: true, characterData: true });
-
-  onStateChange();
+    onStateChange($topics, $addTopics, $jsonFile);
+  }
 })()
